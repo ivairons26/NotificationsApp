6a97cfd9afb363cac2186fb93c77841717ffd899
Iva Zhelyazkova <ivazhelyazkova@Ivas-MacBook-Pro.local>
2024-10-25 10:45:53 +0300

    conditional fields in modal

--- components/ui/add-notification-dialog/AddNotificationDialog.tsx
+++ components/ui/add-notification-dialog/AddNotificationDialog.tsx
@@ -1,59 +1,115 @@
-import React from "react";
+import React, { useState } from "react";
 import * as Dialog from "@radix-ui/react-dialog";
 import { Cross2Icon } from "@radix-ui/react-icons";
+import { SubmitHandler, useForm } from "react-hook-form";
 import "./styles.css";
 import NotificationTypeSelect from "../notification-type-select/NotificationTypeSelect";
 
-const AddNotificationDialog = () => (
-  <Dialog.Root>
-    <Dialog.Trigger asChild>
-      <button className="Button violet">Add</button>
-    </Dialog.Trigger>
-    <Dialog.Portal>
-      <Dialog.Overlay className="DialogOverlay">
-        <Dialog.Content className="DialogContent">
-          <Dialog.Title className="DialogTitle">Add notification</Dialog.Title>
-          <Dialog.Description className="DialogDescription">
-            What kind of notification do you want to add?
-          </Dialog.Description>
-          <fieldset className="Fieldset">
-            <label className="Label" htmlFor="type">
-              Type
-            </label>
-            <NotificationTypeSelect />
-          </fieldset>
+type FormFields = {
+  type: string;
+  name?: string;
+  releaseNumber?: number;
+};
+
+const AddNotificationDialog = () => {
+  const [selectedValue, setSelectedValue] = useState<string>("");
+
+  const { register, handleSubmit } = useForm<FormFields>();
+  const onSubmit: SubmitHandler<FormFields> = (data) => {
+    console.log(data);
+  };
+
+  const onChangeCallback = (change: string) => {
+    setSelectedValue(change);
+  };
+
+  const Item = ({ type }: { type: string }) => {
+    switch (type) {
+      case "chat":
+      case "comment":
+      case "workspace":
+        return (
           <fieldset className="Fieldset">
             <label className="Label" htmlFor="name">
               Name
             </label>
-            <input className="Input" id="name" />
+            <input
+              {...register("name")}
+              type="text"
+              className="Input"
+              id="name"
+            />
           </fieldset>
+        );
+      case "update":
+        return (
           <fieldset className="Fieldset">
             <label className="Label" htmlFor="releasenumber">
               Release number
             </label>
-            <input className="Input" id="releasenumber" />
+            <input
+              {...register("releaseNumber")}
+              type="number"
+              className="Input"
+              id="releasenumber"
+            />
           </fieldset>
-          <div
-            style={{
-              display: "flex",
-              marginTop: 25,
-              justifyContent: "flex-end",
-            }}
-          >
+        );
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <Dialog.Root>
+      <Dialog.Trigger asChild>
+        <button className="Button violet">Add</button>
+      </Dialog.Trigger>
+      <Dialog.Portal>
+        <Dialog.Overlay className="DialogOverlay">
+          <Dialog.Content className="DialogContent">
+            <Dialog.Title className="DialogTitle">
+              Add notification
+            </Dialog.Title>
+            <Dialog.Description className="DialogDescription">
+              What kind of notification do you want to add?
+            </Dialog.Description>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <fieldset className="Fieldset">
+                <label className="Label" htmlFor="type">
+                  Type
+                </label>
+                <NotificationTypeSelect
+                  {...register("type")}
+                  type={selectedValue}
+                  onChange={onChangeCallback}
+                />
+              </fieldset>
+              <Item type={selectedValue} />
+              <div
+                style={{
+                  display: "flex",
+                  marginTop: 25,
+                  justifyContent: "flex-end",
+                }}
+              >
+                {/* <Dialog.Close asChild> */}
+                <button type="submit" className="Button green">
+                  Save changes
+                </button>
+                {/* </Dialog.Close> */}
+              </div>
+            </form>
             <Dialog.Close asChild>
-              <button className="Button green">Save changes</button>
+              <button className="IconButton" aria-label="Close">
+                <Cross2Icon />
+              </button>
             </Dialog.Close>
-          </div>
-          <Dialog.Close asChild>
-            <button className="IconButton" aria-label="Close">
-              <Cross2Icon />
-            </button>
-          </Dialog.Close>
-        </Dialog.Content>
-      </Dialog.Overlay>
-    </Dialog.Portal>
-  </Dialog.Root>
-);
+          </Dialog.Content>
+        </Dialog.Overlay>
+      </Dialog.Portal>
+    </Dialog.Root>
+  );
+};
 
 export default AddNotificationDialog;
--- components/ui/notification-type-select/NotificationTypeSelect.tsx
+++ components/ui/notification-type-select/NotificationTypeSelect.tsx
@@ -1,38 +1,62 @@
 import * as Select from "@radix-ui/react-select";
 import "./styles.css";
-import { ChevronDownIcon } from "@radix-ui/react-icons";
+import { CheckIcon, ChevronDownIcon } from "@radix-ui/react-icons";
+import React from "react";
 
-export const NotificationTypeSelect = () => (
-  <Select.Root>
-    <Select.Trigger className="SelectTrigger" aria-label="type">
-      <Select.Value placeholder="Select a notification type" />
-      <Select.Icon className="SelectIcon">
-        <ChevronDownIcon />
-      </Select.Icon>
-    </Select.Trigger>
-    <Select.Portal>
-      <Select.Content
-        className="SelectContent"
-        position="popper"
-        sideOffset={5}
-      >
-        <Select.Viewport className="SelectViewport">
-          <Select.Item className="SelectItem" value="update">
-            update
-          </Select.Item>
-          <Select.Item className="SelectItem" value="chat">
-            chat
-          </Select.Item>
-          <Select.Item className="SelectItem" value="comment">
-            comment
-          </Select.Item>
-          <Select.Item className="SelectItem" value="workspace">
-            workspace
-          </Select.Item>
-        </Select.Viewport>
-      </Select.Content>
-    </Select.Portal>
-  </Select.Root>
-);
+type Props = {
+  type: string;
+  onChange: (change: string) => void;
+};
+
+export const NotificationTypeSelect = ({ type, onChange }: Props) => {
+  const selectChange = (value: string) => {
+    onChange(value);
+  };
+
+  return (
+    <Select.Root value={type} onValueChange={selectChange}>
+      <Select.Trigger className="SelectTrigger" aria-label="type">
+        <Select.Value placeholder="Select a notification type" />
+        <Select.Icon className="SelectIcon">
+          <ChevronDownIcon />
+        </Select.Icon>
+      </Select.Trigger>
+      <Select.Portal>
+        <Select.Content
+          className="SelectContent"
+          position="popper"
+          sideOffset={5}
+        >
+          <Select.Viewport className="SelectViewport">
+            <Select.Item className="SelectItem" value="update">
+              <Select.ItemText>update</Select.ItemText>
+              <Select.ItemIndicator className="SelectItemIndicator">
+                <CheckIcon />
+              </Select.ItemIndicator>
+            </Select.Item>
+            <Select.Item className="SelectItem" value="chat">
+              <Select.ItemText>chat</Select.ItemText>
+              <Select.ItemIndicator className="SelectItemIndicator">
+                <CheckIcon />
+              </Select.ItemIndicator>
+            </Select.Item>
+            <Select.Item className="SelectItem" value="comment">
+              <Select.ItemText>comment</Select.ItemText>
+              <Select.ItemIndicator className="SelectItemIndicator">
+                <CheckIcon />
+              </Select.ItemIndicator>
+            </Select.Item>
+            <Select.Item className="SelectItem" value="workspace">
+              <Select.ItemText>workspace</Select.ItemText>
+              <Select.ItemIndicator className="SelectItemIndicator">
+                <CheckIcon />
+              </Select.ItemIndicator>
+            </Select.Item>
+          </Select.Viewport>
+        </Select.Content>
+      </Select.Portal>
+    </Select.Root>
+  );
+};
 
 export default NotificationTypeSelect;
