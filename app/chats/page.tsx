8c2657909fed2d26ade3d31c18d5340dca0a546a
Iva Zhelyazkova <iva.a.zhelyazkova@gmail.com>
2024-10-27 16:21:40 +0200

    content placeholder chages for visual ease

--- app/chats/page.tsx
+++ app/chats/page.tsx
@@ -3,8 +3,15 @@ import React from "react";
 function Chats() {
   return (
     <div className="card">
-      <div className="image-placeholder placeholder"></div>
       <div className="title-placeholder placeholder"></div>
+      <div className="text-placeholder placeholder"></div>
+      <div className="text-placeholder placeholder"></div>
+      <div className="text-placeholder placeholder"></div>
+      <div className="text-placeholder placeholder"></div>
+      <div className="text-placeholder placeholder"></div>
+      <div className="text-placeholder placeholder"></div>
+      <div className="text-placeholder placeholder"></div>
+      <div className="text-placeholder placeholder"></div>
       <div className="text-placeholder placeholder"></div>
       <div className="text-placeholder placeholder"></div>
     </div>
--- app/workspace/page.tsx
+++ app/workspace/page.tsx
@@ -3,8 +3,8 @@ import React from "react";
 function Workspace() {
   return (
     <div className="card">
-      <div className="image-placeholder placeholder"></div>
       <div className="title-placeholder placeholder"></div>
+      <div className="image-placeholder placeholder"></div>
       <div className="text-placeholder placeholder"></div>
       <div className="text-placeholder placeholder"></div>
     </div>
