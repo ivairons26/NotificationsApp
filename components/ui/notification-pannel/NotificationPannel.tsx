577bcb7947afeacd63cfa6794749c46bab629c05
Iva Zhelyazkova <iva.a.zhelyazkova@gmail.com>
2024-10-27 14:00:51 +0200

    visual improvements

--- components/ui/notification-pannel/NotificationPannel.tsx
+++ components/ui/notification-pannel/NotificationPannel.tsx
@@ -23,16 +23,14 @@ function NotificationPannel() {
   const users = useContext(UserContext);
 
   const getNotifications = trpc.notification.getNotifications.useQuery();
-  const unreadNotifications = getNotifications.data?.reduce(
-    (count, notification) => {
+  const unreadNotifications =
+    getNotifications.data?.reduce((count, notification) => {
       if (notification.seen === false) {
         count++;
       }
 
       return count;
-    },
-    0
-  );
+    }, 0) || 0;
 
   const notification = trpc.notification.patchNotification.useMutation({
     onError: (err) => {
@@ -51,7 +49,7 @@ function NotificationPannel() {
     <Popover.Root>
       <Popover.Trigger className="flex absolute right-52 top-14 bg-white p-2 rounded-2xl shadow-md">
         <BellIcon />
-        {unreadNotifications && (
+        {unreadNotifications > 0 && (
           <div className="flex items-center justify-center rounded-lg py-0.3 px-1.5 bg-red-500 text-white text-xs">
             <span>{unreadNotifications}</span>
           </div>
