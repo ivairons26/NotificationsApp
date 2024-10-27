d1dd8327a359d92aece8e8cd88923dbee357395d
Iva Zhelyazkova <iva.a.zhelyazkova@gmail.com>
2024-10-27 12:30:04 +0200

    change url to not use localhost in name

--- components/Provider.tsx
+++ components/Provider.tsx
@@ -10,7 +10,7 @@ export default function Provider({ children }: { children: React.ReactNode }) {
     trpc.createClient({
       links: [
         httpBatchLink({
-          url: "http://localhost:3000/api/trpc", // TODO: add this as env var
+          url: "/api/trpc",
         }),
       ],
     })
